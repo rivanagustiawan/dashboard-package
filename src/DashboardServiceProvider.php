e0338eb018dd2c8a54900598bf23c2234364bb45
Your Name <you@example.com>
2022-11-25 07:20:16 +0700

    update providers

--- src/DashboardServiceProvider.php
+++ src/DashboardServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 class DashboardServiceProvider extends ServiceProvider
 {
+    
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
